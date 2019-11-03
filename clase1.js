7e75e4bfc9f2d8f74ebf210e5525fd98ad35d2fe
Luis Gomez <00405018@uca.edu.sv>
2019-11-03 14:26:26 -0600

    Clase 1 de varibales finalizada.

---
+++ clase1.js
@@ -0,0 +1,10 @@
+var nombre = 'Luis' , apellido = 'Gomez'
+var edad = 28
+
+edad = '28 anios'
+peso = 75
+//Por convencion se usa comilla simple aunque se puede usar '', "" y ``
+
+console.log('Hola ' + nombre + ' ' + apellido);
+console.log('Tengo ' + edad);
+
